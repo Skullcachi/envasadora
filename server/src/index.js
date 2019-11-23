5c8d114baf44cf4426aa859c84f7b34829025aa3
Sergio Molina <molinasergio.delta@gmail.com>
2019-11-23 03:47:57 -0600

    added url for ec2

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,8 @@ app.set('port', process.env.PORT || 3000);
 
 //headers para CORS
 var originsWhitelist = [
-    /* 'http://ec2-18-219-165-244.us-east-2.compute.amazonaws.com:3000' */
-     'http://localhost:4200'
+    'http://ec2-18-222-171-178.us-east-2.compute.amazonaws.com:3000'
+     /* 'http://localhost:4200' */
 ];
 var corsOptions = {
 origin: function(origin, callback){
